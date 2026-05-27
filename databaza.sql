6e78e6deb3bb7249f40f7808edd8d692362476e2
bartosjakub33-png <bartosjakub33@gmail.com>
2026-05-27 12:37:45 +0200

    pridal som tabuľku s menom heslom aby som vedel rozlíšiť užívateľov

--- databaza.sql
+++ databaza.sql
@@ -15,10 +15,11 @@ CREATE TABLE tasks (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-INSERT INTO users (username) VALUES 
-('user1'),
-('user2'),
-('user3');
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100),
+    password VARCHAR(255)
+);
 
 INSERT INTO tasks (title, user_id) VALUES 
 ('Task 1', 1),
