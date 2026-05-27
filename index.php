033838ad40c01f0aa3f65c1ec019a9feefd4e7a9
bartosjakub33-png <bartosjakub33@gmail.com>
2026-05-27 12:53:06 +0200

    upravil som index.php aby som sa vedel prihlasiť

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
+
+session_start();
+
+if(!isset($_SESSION["user_id"])){
+    header("Location: login.php");
+    exit();
+}
+
+include "databaza.php";
+
+$user_id = $_SESSION["user_id"];
+
+$query = "
+SELECT *
+FROM tasks
+WHERE user_id = $user_id
+";
+
+$result = mysqli_query($conn, $query);
+
 include "databaza.php";
 
 $query = "
